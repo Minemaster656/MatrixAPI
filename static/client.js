2e41c33f5f25a1ec78d1af8e4dbe5b40dff688e6
Minemaster656 <glebbasmanov123@gmail.com>
2025-06-26 00:23:52 +0400

    new requirements and some other shit

---
+++ static/client.js
@@ -0,0 +1,18 @@
+const testButton = document.getElementById("test-button")
+const testOutput = document.getElementById("test-output")
+
+testButton.addEventListener("click", async ()=>{
+    try {
+        const response = await fetch('/api/quack');
+            if (!response.ok) {
+            throw new Error(`Ошибка HTTP: ${response.status}`);
+            }
+            const data = await response.text(); // или .json(), если сервер возвращает JSON
+            testOutput.innerText = data;
+    } catch (error) {
+        console.error('Ошибка при получении данных:', error);
+        testOutput.innerText = 'Ошибка: ' + error.message;
+    }
+
+
+})
